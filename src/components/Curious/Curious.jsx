34c4046783382abf3cafad4a45b751387435abf2
Amal C P <acp070146@gmail.com>
2023-07-30 02:17:46 +0530

    collaborate section added

---
+++ src/components/Curious/Curious.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Curious = () => {
+  return (
+    <div>
+      <h1>Curious about becoming <span className='color-font'>one of us</span>?</h1>
+    </div>
+  );
+}
+
+export default Curious
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./Home.css";
 import About from "../../components/About/About";
 import VM from "../../components/VM/VM";
+import Curious from "../../components/Curious/Curious";
+
 const Home = () => {
   return (
     <div>
       <About />
       <VM />
+      <Curious />
     </div>
   );
 };
