79d801f7daed6b0444cd6b831610a33a9b943f75
Amal C P <acp070146@gmail.com>
2023-07-30 05:13:39 +0530

    navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,102 @@
-import React from 'react'
-import './Navbar.css'
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+
+import React, { useEffect, useState } from "react";
+import { BiMenu } from "react-icons/bi";
+import {RxCross1} from 'react-icons/rx'
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useReactPath } from "./path.hook";
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+  const [openmenu, setopenmenu] = useState(false);
+  function openMenu() {
+    console.log("true");
+    setopenmenu(!openmenu);
+  }
+  function closeMenu() {
+    setopenmenu(false);
+  }
+  const [navbg, setNavBg] = useState(false);
 
-export default Navbar
+  const path = useReactPath();
+  const navContent = [
+    "ABOUTUS",
+    "PROJECTS",
+    "GOVERNING-BODY",
+    "PARTNERS",
+    "DONATE",
+  ];
+  useEffect(() => {}, [path]);
+  const changeNavBg = () => {
+    window.scrollY >= 150 ? setNavBg(true) : setNavBg(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavBg);
+    return () => {
+      window.removeEventListener("scroll", changeNavBg);
+    };
+  }, []);
+  return (
+    <div className="navbar-wrapper">
+      <div className="navbar">
+        <a href="/">
+          <img className="nav-logo" src={logo} alt="" />
+        </a>
+        <div className="navbar-desk">
+          {navContent.map((content, i) => (
+            <a href={`#${content}`} key={i.toString() + content}>
+              <p
+                style={{
+                  color: window.location.href.includes(`#${content}`)
+                    ? "#FBBA16"
+                    : "",
+                  textTransform: "capitalize",
+                  padding: "5px 10px",
+                  borderRadius: "20px",
+                }}
+              >
+                {content}
+              </p>
+            </a>
+          ))}
+        </div>
+        <div className="navbar-mob">
+          <button
+            style={{ backgroundColor: "transparent", border: "none" }}
+            onClick={openMenu}
+          >
+            {openmenu ? (
+              <RxCross1 style={{ color: "black", fontSize: "40px" }} />
+            ) : (
+              <BiMenu style={{ color: "black", fontSize: "40px" }} />
+            )}
+          </button>
+
+          {openmenu && (
+            <div className="menu-div">
+              {navContent.map((content, i) => (
+                <a href={`#${content}`} key={i.toString() + content}>
+                  <p
+                    style={{
+                      color: window.location.href.includes(`#${content}`)
+                        ? "#FBBA16"
+                        : "",
+                      textTransform: "capitalize",
+                      padding: "5px 10px",
+                      borderRadius: "20px",
+                    }}
+                  >
+                    {content}
+                  </p>
+                </a>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
 import About from "../../components/About/About";
 import VM from "../../components/VM/VM";
 import Curious from "../../components/Curious/Curious";
@@ -7,13 +8,16 @@ import Testimonial from "../../components/Testimonial/Testimonial";
 import Gallery from "../../components/Testimonial/Gallery";
 const Home = () => {
   return (
-    <div>
+    <div className="home-wrapper">
+      <Navbar />
+      <div className="home-screen-wrapper">
+        <h2>Realizing the</h2> <h1>Development Goals</h1>
+      </div>
       <About />
       <VM />
       <Curious />
       <Gallery />
       <Testimonial />
-
     </div>
   );
 };
