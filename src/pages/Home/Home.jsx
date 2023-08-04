bdab8d140d13dffd915dc48775f7c9f42661bf69
Amal C P <acp070146@gmail.com>
2023-08-04 21:28:22 +0530

    home screen update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,11 @@ import Map from "../../components/Map/Map";
 import Footer from "../../components/Footer/Footer";
 import Swiper from "../../components/Testimonial/Slider";
 
+import img1 from "../../assets/home/one.png";
+import img2 from "../../assets/home/two.png";
+import img3 from "../../assets/home/three.png";
+import img4 from "../../assets/home/four.png";
+
 const Home = () => {
   return (
     <div className="home-wrapper">
@@ -24,17 +29,30 @@ const Home = () => {
             <span style={{ color: "#FBBA16" }}>GOALS</span>
           </h1>
         </div>
-        <div>
+        <div className="home-img-button-wrapper">
           <div className="home-button-outer">
             <a href="#ABOUT US">
-              <p>EXPLORE<br></br>UNNATHI</p>
+              <p>
+                EXPLORE<br></br>UNNATHI
+              </p>
               <button style={{ background: "transparent", border: "none" }}>
                 <span class="m_scroll_arrows unu"></span>
                 <span class="m_scroll_arrows doi"></span>
                 <span class="m_scroll_arrows trei"></span>
-               
               </button>
             </a>
+          </div>
+          <div className="home-slider-container">
+            <div className="home-slider-wrapper">
+              <img src={img1} alt="" />
+              <img src={img2} alt="" />
+              <img src={img3} alt="" />
+              <img src={img4} alt="" />
+              <img src={img1} alt="" />
+              <img src={img2} alt="" />
+              <img src={img3} alt="" />
+              <img src={img4} alt="" />
+            </div>
           </div>
         </div>
       </div>
